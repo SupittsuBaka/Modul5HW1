7cd059cd579f4d5f527f6e7c9faab78c2d5ccc3a
Admin-PC\Admin <sashashmidt02@gmail.com>
2021-09-20 17:49:24 +0300

    Создание проекта

---
+++ Modul5HW1/Modul5HW1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Modul5HW1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
